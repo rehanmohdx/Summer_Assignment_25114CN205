5ef54e10e3c85ca7fde7d7819dbda2db70597d99
rehanmohdx <mohdrehands650@gmail.com>
2026-06-04 23:35:50 +0530

    Add Q9 solution

---
+++ Day-3/Q9.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n, i;
+    bool isPrime = true;
+
+    cout << "enter number to be checked whether it is prime or not ";
+    cin >> n;
+
+    for (i = 2; i < n; i++) {
+        if (n % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime)
+        cout << "n is a prime number" << endl;
+    else
+        cout << "n is not a prime number";
+}
