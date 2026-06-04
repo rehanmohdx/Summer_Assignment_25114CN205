fdbecf0c3ad8f0d4f897752c3895398c3dc2c82a
rehanmohdx <mohdrehands650@gmail.com>
2026-06-04 20:12:59 +0530

    Add Q2 solution

---
+++ Day-1/Q2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,i,ans;
+
+    cout << "enter number for which we had to find multiplication table:  " << endl;
+    cin >> n;
+
+    for (i=1; i<=n; i++){
+
+        ans = n*i;
+ cout << n << "*" << i << "=" << ans << endl;
+    }
+    
+}
