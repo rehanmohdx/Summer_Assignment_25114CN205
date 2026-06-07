7b56e188545321baffea18ff05504cc9c34c53ef
rehanmohdx <mohdrehands650@gmail.com>
2026-06-07 19:29:44 +0530

    Add Q8 solution

--- Day-2/Q8.cpp
+++ Day-2/Q8.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n,digit;
-    
-    cout << "enter number whose pallindrome properties to be checked" << endl;
-    cin >>n;
-    
-    while (n!=0){
-        digit = n%10;
-        n = n/100;
-        
-        if (digit==n)
-        {cout <<"n is pallindrome";
+int main() {
+    int n,original,rev=0,digit;
+
+    cout << "Enter number whose palindrome property is to be checked: ";
+    cin >> n;
+
+    original=n;
+
+    while (n != 0) {
+        digit=n%10;
+        rev=rev*10 + digit;
+        n=n/10;
     }
+
+    if (original==rev) {
+        cout << "Number is palindrome";
+    } else {
+        cout << "Number is not palindrome";
+    }
+
+    return 0;
 }
-return 0;
-} 
     
     
     
