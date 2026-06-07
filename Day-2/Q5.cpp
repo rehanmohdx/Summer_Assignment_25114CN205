c1f73d4bece4cbdae803fb1dfa75e3e54b328d01
rehanmohdx <mohdrehands650@gmail.com>
2026-06-08 00:00:32 +0530

    Add Q5 solution

--- Day-2/Q5.cpp
+++ Day-2/Q5.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 
 int main() {
-    int n,i,sum=0;
-    
-    cout << "enter number for which we had to find sum" <<endl;
-    cin >> n;
-    
-    for (i=1;i<=n;i++)
-    {
-        sum=sum+i;
-    }
-    cout << sum;
+    int n,digit,sum=0;
 
-return 0;
+    cout << "Enter number " << endl;
+    cin >> n;
+
+    while (n != 0)
+    {
+        digit=n%10;   
+        sum=sum+digit;
+        n=n/10;      
+    }
+
+    cout << "Sum of digits is " << sum;
+
+    return 0;
 }
